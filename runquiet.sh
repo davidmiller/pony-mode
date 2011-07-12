62f4e038fb959f0e91b159607cd86a922ac61ddb
David Miller <david@deadpansincerity.com>
2011-07-12 19:04:09 +0100

    Adding helper scripts for debugging

---
+++ runquiet.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+emacs --no-desktop -q -Q --load ./tests/resources/init.el --debug-init
---
+++ tests/resources/init.el
@@ -0,0 +1,9 @@
+(add-to-list 'load-path "~/emacs/pony-mode")
+(require 'pony-mode)
+
+(global-set-key [M-left] 'windmove-left) ; move to left windnow
+(global-set-key [M-right] 'windmove-right) ; move to right window
+(global-set-key [M-up] 'windmove-up) ; move to upper window
+(global-set-key [M-down] 'windmove-down) ; move to downer window
+
+(cd "~/")
