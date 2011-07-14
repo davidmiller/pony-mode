323ea26c83736c4173164accf93dee0ce472b919
David Miller <david@deadpansincerity.com>
2011-07-14 09:14:51 +0100

    y-or-n-p in testing emacs -q

--- tests/resources/init.el
+++ tests/resources/init.el
@@ -1,3 +1,8 @@
+;; This file contains a select subsection of the .emacs at
+;; https://github.com/davidmiller/mydotfiles in order to contain the
+;; level of cognitive dissonance incurred when firing up configuration-lite
+;; instances of Emacs for testing purposes
+
 (add-to-list 'load-path "~/emacs/pony-mode")
 (require 'pony-mode)
 
@@ -5,5 +10,5 @@
 (global-set-key [M-right] 'windmove-right) ; move to right window
 (global-set-key [M-up] 'windmove-up) ; move to upper window
 (global-set-key [M-down] 'windmove-down) ; move to downer window
-
+(defalias 'yes-or-no-p 'y-or-n-p)
 (cd "~/")
