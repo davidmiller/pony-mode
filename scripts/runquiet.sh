e776fc34e908d46f55beb4525fdec8dd7d215158
David Miller <david@deadpansincerity.com>
2012-04-08 12:48:14 +0100

    Improve ./scripts/runquiet.sh to take an optional --emacs argument that specifies the Emacs executable to invoke

--- scripts/runquiet.sh
+++ scripts/runquiet.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+EMACS="emacs"
+ARGS=""
 MYPATH=$(dirname $(readlink -f $0))
-emacs --no-desktop -q -Q --load "$MYPATH"/../tests/resources/init.el --debug-init $@
+
+while [ $# -gt 0 ] ; do
+    case $1 in
+
+    --emacs)
+            # The Emacs executable name - useful for x-version testing
+        EMACS=$2
+        shift 2
+        ;;
+
+    *)
+        ARGS="$ARGS $1"
+        shift 1
+        ;;
+    esac
+done
+
+$EMACS --no-desktop -q -Q --load "$MYPATH"/../tests/resources/init.el --debug-init $ARGS
 
