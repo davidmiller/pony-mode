fec2684bb574a63f5ba211c22e05642c08429198
Martin Yrjölä <martin.yrjola@gmail.com>
2014-05-11 12:52:25 +0300

    Fix pony-browser string matching

--- src/pony-mode.el
+++ src/pony-mode.el
@@ -857,8 +857,8 @@ This function allows you to run a server with a 'throwaway' host:port"
           (progn
             (set-buffer "*ponyserver*")
             (goto-char (point-max))
-            (if (search-backward-regexp "Development server is running at \\(.+\\)\n")
-                (setq url (match-string-no-properties 1)))))
+            (if (search-backward-regexp "\\(Starting development server\\|Development server is running\\) at \\(.+\\)\n")
+                (setq url (match-string-no-properties 2)))))
       (pony-runserver))
     (run-with-timer 2 nil 'browse-url url)))
 
