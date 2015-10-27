7c632ad58bc086c989184443228dfed4d141f1da
David Miller <david@deadpansincerity.com>
2015-10-27 15:13:33 +0000

    Fixes highlighting for Form tag.

--- src/pony-tpl.el
+++ src/pony-tpl.el
@@ -33,7 +33,7 @@
 
 (defvar pony-indenting-tags
   '("autoescape" "block" "blocktrans" "comment" "elif" "else" "empty"
-    "filter" "for" "if" "ifchanged" "ifequal" "ifnotequal" "plural" "spaceless" "verbatim" "with")
+    "filter" "form_tag" "for" "if" "ifchanged" "ifequal" "ifnotequal" "plural" "spaceless" "verbatim" "with")
   "List of template tags that imply indentation.")
 
 (defvar pony-indenting-tags-regexp
